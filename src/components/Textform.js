ba2efafee81e6de0b7aa0cfb50229258ac3bc58c
tushar123@eng.rizvi.eng.in <110826068+Tushar-Vaishya@users.noreply.github.com>
2023-04-14 17:23:13 +0530

    Third Commit

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -22,6 +22,11 @@ export default function Textform(props) {
       props.showAlert("Text box has been cleared", "success");
     }
 
+    const handleCopy = () => {
+      navigator.clipboard.writeText(text); 
+      props.showAlert("Copied to Clipboard!", "success");
+    }
+
     const handleExtraSpace = () => {
       let newText = text.split(/[ ]+/);
       setText(newText.join(" "));
@@ -43,13 +48,14 @@ export default function Textform(props) {
       </div>
       <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
       <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to Lowercase</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
       <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
       <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpace}>Remove Extra Space</button>
       
     </div>
     <div className="container my-3" style={{color : props.mode==='dark'?'white':'#042743'}}>
       <h2>Your Text Summary</h2>
-      <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+      <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
       <h2>Preview</h2>
       <p>{text.length>0?text:"Nothing to preview"}</p>
